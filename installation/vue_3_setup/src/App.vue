e175e2dd7780346ffec1c84c5cad561853209d46
Saugat Gurung <gganosh9@gmail.com>
2026-02-27 18:01:32 +0545

    feat: creating components, passing props, reactivity

--- installation/vue_3_setup/src/App.vue
+++ installation/vue_3_setup/src/App.vue
@@ -1,7 +1,22 @@
 <template>
-  <EventDemo />
+  <div>
+    <h1>Parent Component</h1>
+    <ChildComponent 
+      :message="parentMessage" 
+      @update-count="handleUpdate"
+    />
+    <p>Count in Parent: {{ count }}</p>
+  </div>
 </template>
 
 <script setup>
-import EventDemo from './components/EventDemo.vue'
+import { ref } from 'vue'
+import ChildComponent from './components/ChildComponent.vue'
+
+const parentMessage = 'Hello from Parent'
+const count = ref(0)
+
+function handleUpdate(amount) {
+  count.value += amount
+}
 </script>
---
+++ installation/vue_3_setup/src/components/ChildComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+    <p>Message from Parent: {{ message }}</p>
+    <button @click="sendToParent">Increase Count</button>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps({
+  message: String
+})
+
+const emit = defineEmits(['update-count'])
+
+function sendToParent() {
+  emit('update-count', 1)
+}
+</script>
